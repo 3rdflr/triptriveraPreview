aa392dc373cbfdf4d28ec80263ea1d1e6c5d70fc
summerDev96 <167623900+summerDev96@users.noreply.github.com>
2025-08-22 13:47:49 +0900

    chore TRI-9: svgr 삭제, shadCn 및 추가 라이브러리 설치 (#5)
    * chore: shadCn 및 라이브러리 설치
    
    * chore: 폰트 설정 추가

---
+++ src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: '../assets/fonts/PretendardVariable.ttf',
+  variable: '--font-pretendard',
+  weight: '100 900',
+  display: 'swap',
+});
