1e3f7660105c3d35249d421b7574c69697fccc93
summerdev96 <developingsummer96@gmail.com>
2025-08-23 07:41:02 +0900

    chore: 스토리북 예시 수정사항 원복

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -25,13 +25,13 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: '버튼',
+    label: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: '버튼',
+    label: 'Button',
   },
 };
 
