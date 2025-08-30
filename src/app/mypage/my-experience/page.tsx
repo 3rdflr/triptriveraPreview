75da058c1b029da669d58cda84fad8d61d4c2df1
summerdev96 <developingsummer96@gmail.com>
2025-08-30 17:05:49 +0900

    feat: 내 체험 관리 페이지 내 사이드메뉴 컴포넌트 개발

---
+++ src/app/mypage/my-experience/page.tsx
@@ -0,0 +1,5 @@
+const MyExperiencePage = () => {
+  return <div>내 체험 관리 페이지</div>;
+};
+
+export default MyExperiencePage;
---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,12 @@
+import SideMenu from '@/components/pages/myPage/SideMenu';
+
+const MyPage = () => {
+  {
+    /* 모바일에서 마이페이지 링크 */
+  }
+  <div>
+    <SideMenu />
+  </div>;
+};
+
+export default MyPage;
