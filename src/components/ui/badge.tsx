050dc4974d4dd83ac560c238991295885792bcd3
summerdev96 <developingsummer96@gmail.com>
2025-08-31 22:32:33 +0900

    feat: 뱃지 컴포넌트 디자인 수정 및 selected 추가

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils/shadCnUtils';
 
 const badgeVariants = cva(
-  'inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center justify-center rounded-full border px-3.5 py-1.5 text-base font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
   {
     variants: {
       variant: {
@@ -14,7 +14,11 @@ const badgeVariants = cva(
           'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
         destructive:
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline: 'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        outline:
+          'border-[var(--grayscale-100)] text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+      },
+      selected: {
+        true: 'bg-[var(--primary-500)] text-white border-none',
       },
     },
     defaultVariants: {
@@ -26,13 +30,18 @@ const badgeVariants = cva(
 function Badge({
   className,
   variant,
+  selected = false,
   asChild = false,
   ...props
 }: React.ComponentProps<'span'> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
   const Comp = asChild ? Slot : 'span';
 
   return (
-    <Comp data-slot='badge' className={cn(badgeVariants({ variant }), className)} {...props} />
+    <Comp
+      data-slot='badge'
+      className={cn(badgeVariants({ variant, selected }), className)}
+      {...props}
+    />
   );
 }
 
