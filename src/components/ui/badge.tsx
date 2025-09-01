2e5b86fe50cf1b696886fe1dd54e95d73e3dece7
summerdev96 <developingsummer96@gmail.com>
2025-09-01 20:23:19 +0900

    style: 뱃지 size props 추가

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils/shadCnUtils';
 
 const badgeVariants = cva(
-  'inline-flex items-center justify-center rounded-full border px-3.5 py-1.5 text-base font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center justify-center rounded-full border text-base font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
   {
     variants: {
       variant: {
@@ -20,9 +20,14 @@ const badgeVariants = cva(
       selected: {
         true: 'bg-[var(--primary-500)] text-white border-none',
       },
+      size: {
+        xs: 'text-[13px] font-bold px-2 py-1',
+        sm: 'text-base px-3.5 py-1.5',
+      },
     },
     defaultVariants: {
       variant: 'default',
+      size: 'sm',
     },
   },
 );
@@ -30,6 +35,7 @@ const badgeVariants = cva(
 function Badge({
   className,
   variant,
+  size,
   selected = false,
   asChild = false,
   ...props
@@ -39,7 +45,7 @@ function Badge({
   return (
     <Comp
       data-slot='badge'
-      className={cn(badgeVariants({ variant, selected }), className)}
+      className={cn(badgeVariants({ variant, size, selected }), className)}
       {...props}
     />
   );
