958fec5fffb77cac979297f7d3021efb23eb0c05
3rdflr <3rdflrhtl@gmail.com>
2025-09-09 11:13:51 +0900

    fix: 테블릿 -> 모바일 사이즈 744로 수정

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -16,8 +16,8 @@ export function useScreenSize() {
     };
   }, []);
 
-  const isMobile = width < 768;
-  const isTablet = width >= 768 && width < 1024;
+  const isMobile = width < 744;
+  const isTablet = width >= 744 && width < 1024;
   const isDesktop = width >= 1024;
 
   return {
