f48ba26d515b48811bfca1387072d85a1ccc51c8
summerdev96 <developingsummer96@gmail.com>
2025-09-09 23:23:10 +0900

    fix: 드롭다운 빌드 오류 수정

--- src/components/pages/myPage/ScheduleDropdown.tsx
+++ src/components/pages/myPage/ScheduleDropdown.tsx
@@ -1,4 +1,4 @@
-import { Dropdown } from 'react-simplified-package';
+import { Dropdown, useDropdownContext } from 'react-simplified-package';
 import { ChevronDownIcon, CheckIcon } from 'lucide-react';
 import { ReservedSchedule } from '@/types/myReservation.type';
 import { useScreenSize } from '@/hooks/useScreenSize';
@@ -11,41 +11,25 @@ interface ScheduleDropdownProps {
 
 const ScheduleDropdown = ({ value, scheduleList, onChange }: ScheduleDropdownProps) => {
   const { isTablet, isMobile } = useScreenSize();
-  const dropdownWidth = isMobile ? 327 : isTablet ? 338 : 292;
+  const dropdownWidth = isMobile ? 'w-[327px]' : isTablet ? 'w-[338px]' : 'w-[292px]';
   const selected = scheduleList?.find((s) => String(s.scheduleId) === value);
 
   const handleSelect = (val: string) => {
     onChange?.(val);
   };
 
-  return (
-    <Dropdown>
-      <Dropdown.Trigger>
-        <button
-          className='flex justify-between items-center border border-grayscale-100 px-5 py-4 w-full focus:ring-[3px] focus:border-[var(--primary-200)] focus:ring-[var(--primary-200)]/30'
-          style={{
-            width: `${dropdownWidth}px`,
-            borderRadius: '16px',
-          }}
-        >
-          <div className=''>
-            {selected ? `${selected.startTime}-${selected.endTime}` : '예약 시간을 선택해 주세요'}
-          </div>
-          <ChevronDownIcon className='size-4 opacity-50' />
-        </button>
-      </Dropdown.Trigger>
+  const DropdownMenuList = () => {
+    const { close } = useDropdownContext();
 
-      <Dropdown.Menu
-        className='border border-grayscale-100 bg-white w-80 rounded-2xl px-2 py-1.5'
-        style={{
-          width: `${dropdownWidth}px`,
-          borderRadius: '16px',
-        }}
-      >
+    return (
+      <>
         {scheduleList?.map((schedule) => (
           <div
             key={schedule.scheduleId}
-            onClick={() => handleSelect(String(schedule.scheduleId))}
+            onClick={() => {
+              handleSelect(String(schedule.scheduleId));
+              close();
+            }}
             className='hover:bg-primary-100 block rounded-md text-black p-2.5'
           >
             <div className='flex justify-between items-center'>
@@ -56,6 +40,27 @@ const ScheduleDropdown = ({ value, scheduleList, onChange }: ScheduleDropdownPro
             </div>
           </div>
         ))}
+      </>
+    );
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Trigger>
+        <button
+          className={`${dropdownWidth} flex justify-between items-center rounded-2xl border border-grayscale-100 px-5 py-4 focus:ring-[3px] focus:border-[var(--primary-200)] focus:ring-[var(--primary-200)]/30`}
+        >
+          <div className=''>
+            {selected ? `${selected.startTime}-${selected.endTime}` : '예약 시간을 선택해 주세요'}
+          </div>
+          <ChevronDownIcon className='size-4 opacity-50' />
+        </button>
+      </Dropdown.Trigger>
+
+      <Dropdown.Menu
+        className={`${dropdownWidth} border border-grayscale-100 bg-white rounded-2xl px-2 py-1.5`}
+      >
+        <DropdownMenuList />
       </Dropdown.Menu>
     </Dropdown>
   );
