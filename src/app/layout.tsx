b5aa7cb4c25e16a13454d5aeeb014e17b4417901
3rdflr <3rdflrhtl@gmail.com>
2025-09-15 21:17:05 +0900

    feat: speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import { pretendard } from '@/lib/fonts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import ClientLayout from '@/components/common/ClientLayout';
 import './globals.css';
 
@@ -70,6 +71,7 @@ export default function RootLayout({
         <Suspense>
           <ClientLayout>{children}</ClientLayout>
         </Suspense>
+        <SpeedInsights />
       </body>
     </html>
   );
